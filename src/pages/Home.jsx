0cd30f87955dd5bc64f7287661f6f396e53877fe
mamantube <firmanhiday.id007@gmail.com>
2024-06-13 14:59:03 +0700

    add content and styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,41 @@
+import { Row, Col, Card, } from "react-bootstrap";
+import "../assets/css/home.css";
+import emoji from "react-easy-emoji";
+
 export default function Home() {
-    return (
-        <>
-            <h1>Home</h1>
-        </>
-    )
+  return (
+    <main>
+      <Row className=" d-flex justify-content-center align-items-center">
+        <Col className=" p-5">
+          <h4>Hello Every One, My name</h4>
+          <h5 className="my-name">Firman <span className="wave-emoji px-2">{emoji("👋")}</span></h5>
+          <p>
+            A passionate Full Stack Software Developer 🚀 having an experience
+            of building Web applications with JavaScript / Reactjs / Nodejs and
+            some other cool libraries and frameworks.
+          </p>
+        </Col>
+        <Col>
+          <div className=" d-flex justify-content-center align-content-center  my--card">
+            <div className=" card--body">
+              <Row>
+                <Col>
+                  <img className="card--image" src="/img/my-pict.jpg" alt="" />
+                </Col>
+              </Row>
+              <Row>
+                <Col className="social--media">
+                  <a href=""><img src="/img/youtube.png" alt="" /></a>
+                  <a href=""><img src="/img/instagram.png" alt="" /></a>
+                  <a href=""><img src="/img/github.png" alt="" /></a>
+                  <a href=""><img src="/img/linkedin.png" alt="" /></a>
+                  <a href=""><img src="/img/gmail.png" alt="" /></a>
+                </Col>
+              </Row>
+            </div>
+          </div>
+        </Col>
+      </Row>
+    </main>
+  );
 }
