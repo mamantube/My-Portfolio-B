1b0aa401ce24233b27f0731352b02f01b339ce7f
mamantube <firmanhiday.id007@gmail.com>
2024-06-14 16:08:57 +0700

    add footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+        <>
+            <footer className="footer">
+                MAMAN &copy; 2024
+            </footer>
+        </>
+    )
+}
