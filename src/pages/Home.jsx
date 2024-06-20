be540ba9486ca196109a33b860414b3a7717d957
mamantube <firmanhiday.id007@gmail.com>
2024-06-20 15:01:32 +0700

    implementation fade from react-awesome-refeal

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,51 +2,60 @@ import { Row, Col } from "react-bootstrap";
 import "../assets/css/home.css";
 import emoji from "react-easy-emoji";
 import Skillset from "../components/Skillset";
+import { Fade } from "react-awesome-reveal";
 
 export default function Home() {
   return (
     <main>
       <Row className=" d-flex justify-content-center align-items-center">
         <Col className=" p-5">
-          <h4>Hello Every One, My name</h4>
-          <h5 className="my-name">
-            Firman <span className="wave-emoji px-2">{emoji("👋")}</span>
-          </h5>
-          <p>
-            A passionate Full Stack Software Developer 🚀 having an experience
-            of building Web applications with JavaScript / Reactjs / Nodejs and
-            some other cool libraries and frameworks.
-          </p>
+          <Fade direction="left" duration="1500">
+            <h4>Hello Every One, My name</h4>
+            <h5 className="my-name">
+              Firman <span className="wave-emoji px-2">{emoji("👋")}</span>
+            </h5>
+            <p>
+              A passionate Full Stack Software Developer 🚀 having an experience
+              of building Web applications with JavaScript / Reactjs / Nodejs
+              and some other cool libraries and frameworks.
+            </p>
+          </Fade>
         </Col>
         <Col className=" d-flex justify-content-center align-items-center profile--picture">
-          <div className=" align-content-center my--card">
-            <div className=" card--body">
-              <Row>
-                <Col>
-                  <img className="card--image" src="/img/my-pict.jpg" alt="" />
-                </Col>
-              </Row>
-              <Row>
-                <Col className="social--media">
-                  <a href="">
-                    <img src="/img/youtube.png" alt="" />
-                  </a>
-                  <a href="">
-                    <img src="/img/instagram.png" alt="" />
-                  </a>
-                  <a href="">
-                    <img src="/img/github.png" alt="" />
-                  </a>
-                  <a href="">
-                    <img src="/img/linkedin.png" alt="" />
-                  </a>
-                  <a href="">
-                    <img src="/img/gmail.png" alt="" />
-                  </a>
-                </Col>
-              </Row>
+          <Fade direction="right" duration="1500">
+            <div className=" align-content-center my--card">
+              <div className=" card--body">
+                <Row>
+                  <Col>
+                    <img
+                      className="card--image"
+                      src="/img/my-pict.jpg"
+                      alt=""
+                    />
+                  </Col>
+                </Row>
+                <Row>
+                  <Col className="social--media">
+                    <a href="">
+                      <img src="/img/youtube.png" alt="" />
+                    </a>
+                    <a href="">
+                      <img src="/img/instagram.png" alt="" />
+                    </a>
+                    <a href="">
+                      <img src="/img/github.png" alt="" />
+                    </a>
+                    <a href="">
+                      <img src="/img/linkedin.png" alt="" />
+                    </a>
+                    <a href="">
+                      <img src="/img/gmail.png" alt="" />
+                    </a>
+                  </Col>
+                </Row>
+              </div>
             </div>
-          </div>
+          </Fade>
         </Col>
       </Row>
       <Skillset />
