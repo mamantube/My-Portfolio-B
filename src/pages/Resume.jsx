e5408434bbaf02ba0ee3cdad0ea15cdefdcb8042
mamantube <firmanhiday.id007@gmail.com>
2024-06-23 22:15:21 +0700

    add cv image

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,45 +1,21 @@
-/* eslint-disable no-undef */
+import { Col, Row } from "react-bootstrap";
 import "../assets/css/resume.css";
-import { Row, Col } from "react-bootstrap";
-// import myresume from "../assets/my-resume.pdf"; // pastikan jalur ini benar
-import { Document, Page, pdfjs } from "react-pdf";
-import { useState, useEffect } from "react";
-import "react-pdf/dist/esm/Page/AnnotationLayer.css";
-
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  'pdfjs-dist/build/pdf.worker.min.js',
-  import.meta.url,
-).toString();
 
 export default function Resume() {
-  const [width, setWidth] = useState(1200);
-
-  useEffect(() => {
-    const handleResize = () => setWidth(window.innerWidth);
-    window.addEventListener("resize", handleResize);
-    handleResize();
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-
   return (
     <section>
-      <Row>
-        <Col className="d-flex justify-content-center align-items-center">
-          <h1>MY RESUME</h1>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <Document file="my-resume.pdf" className="d-flex justify-content-center">
-            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
-          </Document>
-        </Col>
-      </Row>
-      <Row>
-        <Col className="d-flex justify-content-center align-items-center">
-          <h1>CERTIFICATION</h1>
-        </Col>
-      </Row>
+      <div>
+        <Row>
+          <Col className=" d-flex justify-content-center align-items-center mt-5">
+            <h1>MY RESUME</h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col className=" d-flex justify-content-center align-items-center mt-5">
+            <img className="cv-img" src="/img/my-resume.png" alt="" />
+          </Col>
+        </Row>
+      </div>
     </section>
   );
 }
