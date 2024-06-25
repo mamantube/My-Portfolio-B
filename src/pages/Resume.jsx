ac460c6f1622e068ee62b4eca3f42249f705994b
mamantube <firmanhiday.id007@gmail.com>
2024-06-25 09:47:41 +0700

    add CV on resume page

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,5 +1,14 @@
-import { Col, Row } from "react-bootstrap";
+import { Col, Row, Button } from "react-bootstrap";
 import "../assets/css/resume.css";
+import pdf from "../assets/my-resume.pdf";
+import { Document, Page, pdfjs } from "react-pdf";
+import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
+import 'react-pdf/dist/esm/Page/TextLayer.css';
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  'pdfjs-dist/build/pdf.worker.min.mjs',
+  import.meta.url,
+).toString();
+
 
 export default function Resume() {
   return (
@@ -11,9 +20,17 @@ export default function Resume() {
           </Col>
         </Row>
         <Row>
-          <Col className=" d-flex justify-content-center align-items-center mt-5">
-            <img className="cv-img" src="/img/my-resume.png" alt="" />
+          <Col className=" d-flex justify-content-center align-items-center mt-4">
+            {/* <img className="cv-img" src="/img/my-resume.png" alt="" /> */}
+            <Document file={pdf} className="d-flex justify-content-center cv-img">
+              <Page pageNumber={1} />
+            </Document>
           </Col>
+          <Row>
+            <Col className=" d-flex justify-content-center align-items-center mt-5">
+              <Button variant="outline-primary" href={pdf} target="_blank">Download CV</Button>
+            </Col>
+          </Row>
         </Row>
       </div>
     </section>
